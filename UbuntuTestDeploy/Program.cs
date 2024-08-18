ad4b4cf20adb2d9170d3f4b81dda865946907ba7
Vyacheslav <gold.like.simple@gmail.com>
2024-08-18 18:55:45 +0300

    [+] new hello world

--- UbuntuTestDeploy/Program.cs
+++ UbuntuTestDeploy/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! From CI/CD ____ !!!@#!");
+app.MapGet("/", () => "Hello World! From CI/CD");
 
 app.Run();
