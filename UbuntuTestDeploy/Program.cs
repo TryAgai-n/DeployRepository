10d91358e65a2c7676e557c593cbdb2dd95c68c7
Vyacheslav <gold.like.simple@gmail.com>
2024-08-18 18:14:15 +0300

    [fix]

--- UbuntuTestDeploy/Program.cs
+++ UbuntuTestDeploy/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! From CI/CD");
+app.MapGet("/", () => "Hello World! From CI/CD ____ !!!@#!");
 
 app.Run();
